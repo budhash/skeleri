348e04dabca59ceb3a948655b8d60bc50283c02e
budhash <budhash@gmail.com>
2023-01-16 21:11:56 -0800

    updated build.gradle.kts to display test stats

--- template/gradle/proj.build.gradle.kts
+++ template/gradle/proj.build.gradle.kts
@@ -30,6 +30,72 @@ kotlin {
     jvmToolchain(11)
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform {
+        // configuration parameters to execute top-level classes in sequentially but their methods in parallel
+        systemProperties = mapOf(
+            "junit.jupiter.execution.parallel.enabled" to true,
+            "junit.jupiter.execution.parallel.mode.default" to "concurrent",
+            "junit.jupiter.execution.parallel.mode.classes.default" to "concurrent",
+            "junit.jupiter.execution.parallel.config.strategy" to "dynamic",
+            "junit.jupiter.execution.parallel.config.dynamic.factor" to 2
+        )
+    }
+
+    testLogging.showStandardStreams = false
+
+    testLogging {
+        showStandardStreams = false
+        showStackTraces = true
+        displayGranularity = 1
+        events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        exceptionFormat = TestExceptionFormat.FULL
+
+        info {
+            events = setOf(TestLogEvent.STARTED, TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+
+    val failedTests = mutableListOf<TestDescriptor>()
+    val skippedTests = mutableListOf<TestDescriptor>()
+
+    // See https://github.com/gradle/gradle/issues/5431
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+            when (result.resultType) {
+                TestResult.ResultType.FAILURE -> failedTests.add(testDescriptor)
+                TestResult.ResultType.SKIPPED -> skippedTests.add(testDescriptor)
+                else -> Unit
+            }
+        }
+
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) { // root suite
+                logger.lifecycle("----")
+                logger.lifecycle("Test result: ${result.resultType}")
+                logger.lifecycle(
+                    "Test summary: ${result.testCount} tests, " +
+                        "${result.successfulTestCount} succeeded, " +
+                        "${result.failedTestCount} failed, " +
+                        "${result.skippedTestCount} skipped"
+                )
+                failedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tFailed Tests")
+                skippedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tSkipped Tests:")
+            }
+        }
+
+        private infix fun List<TestDescriptor>.prefixedSummary(subject: String) {
+            logger.lifecycle(subject)
+            forEach { test -> logger.lifecycle("\t\t${test.displayName()}") }
+        }
+
+        private fun TestDescriptor.displayName() = parent?.let { "${it.name} - $name" } ?: "$name"
+    })
+}
+
 tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest {
